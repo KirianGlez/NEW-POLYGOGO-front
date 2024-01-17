9b28a4eaf157bb62d70de95eade59d730e8349c1
Beneharo Bentor Diaz Medina <beneharodm@gmail.com>
2024-01-17 00:57:47 +0000

    MessageRegister
    Sencillito chatopenai

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
+
 export class RegisterComponent {
   username: string = '';
   password: string = '';
+  showSuccessPopup: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -16,12 +19,17 @@ export class RegisterComponent {
     this.authService.register(this.username, this.password).subscribe(
       (data) => {
         console.log('Registro exitoso:', data);
-        this.router.navigate(['/login']);
+        this.showSuccessPopup = true;
       },
       (error) => {
         console.error('Error en registro:', error);
         // Mostrar mensaje de error al usuario
       }
     );
+  }
+
+  closeSuccessPopup() {
+    this.showSuccessPopup = false;
+    this.router.navigate(['/login']);
   }
 }
